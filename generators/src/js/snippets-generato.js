06be3687e094dccb0f830577c739d1dc4559193a
Eliseo Geraldo Gonzalez <e10169610@gmail.com>
2017-12-27 08:07:48 -0700

    snippets iterator

--- generators/src/js/snippets-generato.js
+++ generators/src/js/snippets-generato.js
@@ -7,8 +7,27 @@ document.addEventListener('DOMContentLoaded', () => {
 			this.cahceDOM()
 			this.bindListeners()
 		},
+		displaySettgins () {
+			this.$settings.classList.toggle('active')
+			if (this.$settings.classList.contains('active')) {
+				this.$settings.style.transition = '0.3s ease'
+				this.$settings.style.transform = 'rotate(90deg)'
+				this.$settings.style.top = '80px'
+
+				this.$textarea.style.transition = '0.3s ease'
+				this.$textarea.style.top = '60px'
+
+				this.$mirror.style.transition = '0.3s ease'
+				this.$mirror.style.top = '60px'
+			} else {
+				this.$textarea.style.top = '0px'
+				this.$mirror.style.top = '0px'
+
+				this.$settings.style.top = '20px'
+				this.$settings.style.transform = 'rotate(0deg)'
+			}
+		},
 		highlit () {
-			c('round')
 			this.text = this.$textarea.value
 			this.text = this.text.replace(/</g, '&lt;')
 			this.text = this.text.replace(/>/g, '&gt;')
@@ -16,23 +35,16 @@ document.addEventListener('DOMContentLoaded', () => {
 			this.text = this.text.replace(/((Text|Snippet|Image|Page)*\..*\$)/g, '<yellow>$1</yellow>')
 			//this.text = this.text.replace(/(&lt;.* )(.*=".*")(\/?&gt;)/g, '$1<light>$2</light>$3')
 			this.$mirror.innerHTML = this.text.replace(/(\$)/g, '<purple>$1</purple>')
-
 			this.secondProccess = this.$mirror.textContent.replace(/((Text|Snippet|Image|PageSnippet)\..*\$)/g, '[[$1]]')
+
 			this.temp = ''
+
 			for (let i = 1; i <= parseInt(this.$qutty.value); i++) {
-
-				c('rounded')
-
 				this.$qwe = parseInt(this.$qutty.value)
-				c( this.$qwe > 9 && this.$qwe <= 99 )
-				c( typeof this.$qwe )
-
 				if ( this.$qwe > 0 && this.$qwe <= 9 ) {
 					this.temp += this.secondProccess.replace(/\$/g, `${i}`)
 				}
-
 				if ( this.$qwe > 9 && this.$qwe <= 99 ) {
-
 					if ( i > 9 ) {
 						this.temp += this.secondProccess.replace(/\$/g, `${i}`)
 					} else {
@@ -40,7 +52,6 @@ document.addEventListener('DOMContentLoaded', () => {
 					}
 
 				}
-
 				if ( this.$qwe > 99 && this.$qwe <= 999 ) {
 
 					if ( i > 99 ) {
@@ -50,18 +61,7 @@ document.addEventListener('DOMContentLoaded', () => {
 					} else {
 						this.temp += this.secondProccess.replace(/\$/g, `00${i}`)
 					}
-
 				}
-
-				/*if ( this.$qutty.value > 9 ) {
-					this.temp += this.secondProccess.replace(/\$/g, `0${i}`)
-				} else if ( this.$qutty.value > 99 ) {
-					this.temp += this.secondProccess.replace(/\$/g, `00${i}`)
-				} else if ( this.$qutty.value > 999 ) {
-					this.temp += this.secondProccess.replace(/\$/g, `00${i}`)
-				}*/
-
-
 				this.temp += '\n'
 			}
 			this.$screen.textContent = this.temp
@@ -69,13 +69,14 @@ document.addEventListener('DOMContentLoaded', () => {
 		bindListeners () {
 			this.$textarea.addEventListener('input', this.highlit.bind(this))
 			this.$qutty.addEventListener('input', this.highlit.bind(this))
+			this.$settings.addEventListener('click', this.displaySettgins.bind(this))
 		},
 		cahceDOM () {
 			this.$mirror = document.querySelector('md-mirror')
 			this.$textarea = document.querySelector('#snippetGenerator')
 			this.$screen = document.querySelector('md-screen')
 			this.$qutty = document.querySelector('[name=qutty]')
-			c( this.$textarea )
+			this.$settings = document.querySelector('.settings-icon')
 		}
 	}
 
